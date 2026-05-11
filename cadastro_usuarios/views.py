5b0d4503f046f612f3aff53dc0daf01264bf4be2
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2026-05-11 17:49:09 -0300

    Aula do dia 11/05/2026.

--- cadastro_usuarios/views.py
+++ cadastro_usuarios/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Usuario
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    usuarios = Usuario.objects.all()
+    return render(request, "index.html", {"usuarios":usuarios})
+
+def form(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cpf = request.POST.get('cpf')
+        email = request.POST.get('email')
+        genero = request.POST.get('genero')
+        Usuario.objects.create(nome=nome, cpf=cpf, email=email, genero=genero)
+        return redirect('index')
+    return render(request, 'form.html')
